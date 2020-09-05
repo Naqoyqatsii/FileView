98cdf501bc7db8bda30d4f49cb245ca0b7401af6
Koyaanisqatsi <827779@gmail.com>
2020-09-05 18:32:39 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsDZ060901
+{
+	partial class File_Viewer
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.DirTree = new System.Windows.Forms.TreeView();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this._Root = new System.Windows.Forms.ToolStripMenuItem();
+			this._Table = new System.Windows.Forms.ToolStripMenuItem();
+			this._List = new System.Windows.Forms.ToolStripMenuItem();
+			this._Little = new System.Windows.Forms.ToolStripMenuItem();
+			this._Normal = new System.Windows.Forms.ToolStripMenuItem();
+			this._Big = new System.Windows.Forms.ToolStripMenuItem();
+			this.ViewFile = new System.Windows.Forms.ListView();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// DirTree
+			// 
+			this.DirTree.Dock = System.Windows.Forms.DockStyle.Left;
+			this.DirTree.Location = new System.Drawing.Point(0, 31);
+			this.DirTree.Name = "DirTree";
+			this.DirTree.Size = new System.Drawing.Size(369, 683);
+			this.DirTree.TabIndex = 0;
+			this.DirTree.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.DirTree_BeforeExpand);
+			this.DirTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.DirTree_AfterSelect);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(22, 22);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._Root});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1244, 31);
+			this.menuStrip1.TabIndex = 2;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// _Root
+			// 
+			this._Root.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._Table,
+            this._List,
+            this._Little,
+            this._Normal,
+            this._Big});
+			this._Root.Name = "_Root";
+			this._Root.Size = new System.Drawing.Size(55, 27);
+			this._Root.Text = "Вид";
+			// 
+			// _Table
+			// 
+			this._Table.Name = "_Table";
+			this._Table.Size = new System.Drawing.Size(266, 30);
+			this._Table.Text = "Таблица";
+			this._Table.Click += new System.EventHandler(this._Table_Click);
+			// 
+			// _List
+			// 
+			this._List.Name = "_List";
+			this._List.Size = new System.Drawing.Size(266, 30);
+			this._List.Text = "Список";
+			this._List.Click += new System.EventHandler(this._List_Click);
+			// 
+			// _Little
+			// 
+			this._Little.Name = "_Little";
+			this._Little.Size = new System.Drawing.Size(266, 30);
+			this._Little.Text = "Мелкие значки";
+			this._Little.Click += new System.EventHandler(this._Little_Click);
+			// 
+			// _Normal
+			// 
+			this._Normal.Name = "_Normal";
+			this._Normal.Size = new System.Drawing.Size(266, 30);
+			this._Normal.Text = "Нормальные значки";
+			this._Normal.Click += new System.EventHandler(this._Normal_Click);
+			// 
+			// _Big
+			// 
+			this._Big.Name = "_Big";
+			this._Big.Size = new System.Drawing.Size(266, 30);
+			this._Big.Text = "Крупные значки";
+			this._Big.Click += new System.EventHandler(this._Big_Click);
+			// 
+			// ViewFile
+			// 
+			this.ViewFile.Dock = System.Windows.Forms.DockStyle.Right;
+			this.ViewFile.HideSelection = false;
+			this.ViewFile.Location = new System.Drawing.Point(375, 31);
+			this.ViewFile.Name = "ViewFile";
+			this.ViewFile.Size = new System.Drawing.Size(869, 683);
+			this.ViewFile.TabIndex = 3;
+			this.ViewFile.UseCompatibleStateImageBehavior = false;
+			this.ViewFile.View = System.Windows.Forms.View.Details;
+			// 
+			// File_Viewer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1244, 714);
+			this.Controls.Add(this.ViewFile);
+			this.Controls.Add(this.DirTree);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "File_Viewer";
+			this.Text = "Обзор файлов";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TreeView DirTree;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem _Root;
+		private System.Windows.Forms.ListView ViewFile;
+		private System.Windows.Forms.ToolStripMenuItem _Table;
+		private System.Windows.Forms.ToolStripMenuItem _List;
+		private System.Windows.Forms.ToolStripMenuItem _Little;
+		private System.Windows.Forms.ToolStripMenuItem _Normal;
+		private System.Windows.Forms.ToolStripMenuItem _Big;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsDZ060901
+{
+	public partial class File_Viewer : Form
+	{
+		DirectoryInfo dirinfo;
+		public File_Viewer()
+		{
+			InitializeComponent();
+
+			var drive = DriveInfo.GetDrives();
+
+			DirTree.Nodes.Clear();
+
+			foreach (var el in drive)
+			{
+				DirTree.Nodes.Add(el.Name).Tag = el.RootDirectory;
+			}
+
+			Fill_nodes(DirTree.Nodes);
+
+			ViewFile.View = View.Details;
+		}
+
+		private void Fill_nodes(TreeNodeCollection nodes)
+		{
+			foreach (TreeNode el in nodes)
+			{
+				try
+				{
+					var dirs = ((DirectoryInfo)el.Tag).GetDirectories();
+
+					foreach (var dir in dirs)
+					{
+						el.Nodes.Add(dir.Name).Tag = dir;
+					}
+				}
+				catch { }
+			}
+		}
+
+		private void DirTree_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+		{
+			Fill_nodes(e.Node.Nodes);
+		}
+
+		private void DirTree_AfterSelect(object sender, TreeViewEventArgs e)
+		{
+			dirinfo = (DirectoryInfo)e.Node.Tag;
+			var files = dirinfo.GetFiles();
+			ViewFile.View = View.Details;
+
+			ViewFile.Items.Clear();
+			ViewFile.Columns.Clear();
+			ViewFile.Columns.Add("Имя файла").Width = 200;
+			ViewFile.Columns.Add("Размер").Width = 200;
+			ViewFile.Columns.Add("Дата создания").Width = 200;
+			
+			foreach (FileInfo file in files)
+			{
+				var i = ViewFile.Items.Add(file.Name);
+				i.SubItems.Add(file.Length.ToString());
+				i.SubItems.Add(file.CreationTime.ToString());
+			}
+		}
+
+		private void _Table_Click(object sender, EventArgs e)
+		{
+			ViewFile.View = View.Details;
+			var files = dirinfo.GetFiles();
+			ViewFile.View = View.Details;
+
+			ViewFile.Items.Clear();
+			ViewFile.Columns.Clear();
+			ViewFile.Columns.Add("Имя файла").Width = 200;
+			ViewFile.Columns.Add("Размер").Width = 200;
+			ViewFile.Columns.Add("Дата создания").Width = 200;
+
+			foreach (FileInfo file in files)
+			{
+				var i = ViewFile.Items.Add(file.Name);
+				i.SubItems.Add(file.Length.ToString());
+				i.SubItems.Add(file.CreationTime.ToString());
+			}
+		}
+
+		private void _List_Click(object sender, EventArgs e)
+		{
+			var files = dirinfo.GetFiles();
+			ViewFile.Items.Clear();
+			ViewFile.Columns.Clear();
+			ViewFile.View = View.List;
+
+			foreach (FileInfo file in files)
+			{
+				ViewFile.Items.Add(file.Name);
+			}
+		}
+
+		private void _Little_Click(object sender, EventArgs e)
+		{
+			var files = dirinfo.GetFiles();
+
+			ViewFile.Items.Clear();
+			ViewFile.Columns.Clear();
+
+			ViewFile.View = View.SmallIcon;
+			ImageList imageList = new ImageList();
+			imageList.ImageSize = new Size(16, 16);
+			
+			foreach (var file in files)
+			{
+				if (file.Extension == ".jpg" || file.Extension == ".bmp" || file.Extension == ".jpeg")
+					imageList.Images.Add(file.FullName, Image.FromFile(file.FullName));
+				else
+					imageList.Images.Add(file.FullName, Icon.ExtractAssociatedIcon(file.FullName));
+			}
+
+			ViewFile.SmallImageList = imageList;
+
+			foreach (FileInfo file in files)
+			{
+				ViewFile.Items.Add(file.Name, imageList.Images.IndexOfKey(file.FullName));
+			}
+		}
+
+		private void _Big_Click(object sender, EventArgs e)
+		{
+			var files = dirinfo.GetFiles();
+
+			ViewFile.Items.Clear();
+			ViewFile.Columns.Clear();
+
+			ViewFile.View = View.LargeIcon;
+			ImageList imageList = new ImageList();
+			imageList.ImageSize = new Size(60, 60);
+
+			foreach (var file in files)
+			{
+				if (file.Extension == ".jpg" || file.Extension == ".bmp" || file.Extension == ".jpeg")
+					imageList.Images.Add(file.FullName, Image.FromFile(file.FullName));
+				else
+					imageList.Images.Add(file.FullName, Icon.ExtractAssociatedIcon(file.FullName));
+			}
+
+			ViewFile.LargeImageList = imageList;
+
+			foreach (FileInfo file in files)
+			{
+					ViewFile.Items.Add(file.Name, imageList.Images.IndexOfKey(file.FullName));
+			}
+		}
+
+		private void _Normal_Click(object sender, EventArgs e)
+		{
+			var files = dirinfo.GetFiles();
+
+			ViewFile.Items.Clear();
+			ViewFile.Columns.Clear();
+
+			ViewFile.View = View.SmallIcon;
+			ImageList imageList = new ImageList();
+			imageList.ImageSize = new Size(30, 30);
+			
+			foreach (var file in files)
+			{
+				if (file.Extension == ".jpg" || file.Extension == ".bmp" || file.Extension == ".jpeg")
+					imageList.Images.Add(file.FullName, Image.FromFile(file.FullName));
+				else
+					imageList.Images.Add(file.FullName, Icon.ExtractAssociatedIcon(file.FullName));
+			}
+
+			ViewFile.SmallImageList = imageList;
+
+			foreach (FileInfo file in files)
+			{
+				ViewFile.Items.Add(file.Name, imageList.Images.IndexOfKey(file.FullName));
+			}
+		}
+	}
+}
